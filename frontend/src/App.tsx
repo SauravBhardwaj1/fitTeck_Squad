99a5aaeb0619ead0ce4ade85f4fa03b3f29d6f55
Ajay84sia <ajay9679339@gmail.com>
2023-05-11 19:24:42 +0530

    login & register page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Login/>
+      <Register/>
     </div>
   );
 }
